06f7da554092c70f134e870abedfc9d5ec08c269
SamanthaClaes <Samantha.claes@student.hepl.be>
2025-11-28 21:35:47 +0100

    add table on table animals page

--- resources/views/admin/animal.blade.php
+++ resources/views/admin/animal.blade.php
@@ -39,26 +39,134 @@
                         <td class="px-4 py-4 border-r-1 border-b-1">Disponible</td>
                         <td class="px-4 py-4 border-r-1 border-b-1">Validée</td>
                         <td class="px-4 py-4 border-r-1 border-b-1">
-                            <a href="">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-text">
-                                    <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-
-                                </svg>
-                            </a>
+                            <x-SVG.pen/>
                         </td>
-
                     </tr>
                     <tr>
-                        <td class="px-4 py-4 border-r-1">Newton</td>
-                        <td class="px-4 py-4 border-r-1">Golden Retriever</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Newton</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Golden Retriever</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Disponible</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Validée</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">
+                            <x-SVG.pen/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Freddy</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Bouledogue Français</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Disponible</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Validée</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">
+                            <x-SVG.pen/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Archibald</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Siamois</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Disponible</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Validée</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">
+                            <x-SVG.pen/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-4 py-4 border-r-1 ">Moka</td>
+                        <td class="px-4 py-4 border-r-1">Caniche</td>
                         <td class="px-4 py-4 border-r-1">Disponible</td>
                         <td class="px-4 py-4 border-r-1">Validée</td>
                         <td class="px-4 py-4 border-r-1 border-b-1">
-                            <a href="">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-text">
-                                    <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-                                </svg>
-                            </a>
+                            <x-SVG.pen/>
+                        </td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="flex justify-between items-center mt-8">
+                <h2 class="pt-8 font-semibold text-text text-xl pb-4">Liste des animaux en cours d'adoption</h2>
+                <a href="" class="bg-cta p-2 h-10 rounded-xl text-white hover:bg-hover">
+                    + ajouter un animal
+                </a>
+            </div>
+            <div class="p-4 bg-element rounded-2xl">
+                <table class="border-1 w-full">
+                    <thead>
+                    <tr class="bg-gray-50 border-b-1">
+                        <th class="border-r-1">Nom</th>
+                        <th class="border-r-1">Espèce</th>
+                        <th class="border-r-1">Adoption</th>
+                        <th class="border-r-1">Fiche</th>
+                        <th class="border-r-1">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Garfield</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">British shortair</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">En cours</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Validée</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">
+                            <x-SVG.pen/>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="flex justify-between items-center mt-8">
+                <h2 class="pt-8 font-semibold text-text text-xl pb-4">Liste des animaux en soins</h2>
+                <a href="" class="bg-cta p-2 h-10 rounded-xl text-white hover:bg-hover">
+                    + ajouter un animal
+                </a>
+            </div>
+            <div class="p-4 bg-element rounded-2xl">
+                <table class="border-1 w-full">
+                    <thead>
+                    <tr class="bg-gray-50 border-b-1">
+                        <th class="border-r-1">Nom</th>
+                        <th class="border-r-1">Espèce</th>
+                        <th class="border-r-1">Adoption</th>
+                        <th class="border-r-1">Fiche</th>
+                        <th class="border-r-1">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Billy</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Chihuahua</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">En soins</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Validée</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">
+                            <x-SVG.pen/>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="flex justify-between items-center mt-8">
+                <h2 class="pt-8 font-semibold text-text text-xl pb-4">Liste des animaux adoptés</h2>
+                <a href="" class="bg-cta p-2 h-10 rounded-xl text-white hover:bg-hover">
+                    + ajouter un animal
+                </a>
+            </div>
+            <div class="p-4 bg-element rounded-2xl">
+                <table class="border-1 w-full">
+                    <thead>
+                    <tr class="bg-gray-50 border-b-1">
+                        <th class="border-r-1">Nom</th>
+                        <th class="border-r-1">Espèce</th>
+                        <th class="border-r-1">Adoption</th>
+                        <th class="border-r-1">Fiche</th>
+                        <th class="border-r-1">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Cookie</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Européen</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Adoptée</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">Validée</td>
+                        <td class="px-4 py-4 border-r-1 border-b-1">
+                            <x-SVG.pen/>
                         </td>
                     </tr>
                     </tbody>
