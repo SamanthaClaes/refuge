771361d50ea71100c14d95bfdd395858d44f0030
SamanthaClaes <Samantha.claes@student.hepl.be>
2026-01-02 23:18:51 +0100

    try to see my animals pictures

--- resources/views/components/cards/animal-card.blade.php
+++ resources/views/components/cards/animal-card.blade.php
@@ -13,7 +13,7 @@
     <div class="bg-element h-auto rounded-xl p-3 w-full transition-transform duration-400 hover:scale-105 ">
         <div class="pl-2 pt-2 pr-2 pb-6 relative ">
             <img
-                src="{{ $avatar ? asset('storage/avatars/original/' . basename($avatar)) : '' }}"
+                src="{{ $avatar ? asset('storage/' . $avatar) : '' }}"
                 alt="{{ $name }}"
                 class="rounded-lg w-full h-64 object-cover reveal-on-scroll"
             >
